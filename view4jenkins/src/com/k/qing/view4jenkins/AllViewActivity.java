d325328f753a403fc9491bf6c2c63f91dff8cd40
ejiaqsu <jiaqing.sun@ericsson.com>
2013-09-09 17:33:30 +0800

    ssdfd

--- view4jenkins/src/com/k/qing/view4jenkins/AllViewActivity.java
+++ view4jenkins/src/com/k/qing/view4jenkins/AllViewActivity.java
@@ -1,25 +1,26 @@
 package com.k.qing.view4jenkins;
 
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.BaseAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ExpandableListView;
-import android.widget.TabHost;
 import android.widget.TextView;
 
 import com.k.qing.view4jenkins.bean.JViewer;
@@ -29,12 +30,7 @@ import com.k.qing.view4jenkins.util.JenkinsJsonParser;
 
 public class AllViewActivity extends Activity {
 
-	private List<String> groupData;
-	private List<List<String>> childrenData;
-	
 	private JViewer jViewer;
-	
-	final ExpandableAdapter expandableAdapter = new ExpandableAdapter();
 	
 	private Handler handler = new Handler() {
 		public void handleMessage(Message msg) {
@@ -47,25 +43,12 @@ public class AllViewActivity extends Activity {
 		};
 	};
 	
+	
 	public static ProgressDialog progressDialog;
 	
 	public static String jenkinsURL = "http://147.128.17.152:8080/jenkins/";
 	
 	private Handler progressDialogHandler = new AllViewHandler(this);
-	
-	private void initTabHost() {
-		TabHost tabHost = (TabHost) findViewById(R.id.myTabHost);
-
-		this.initExpandableData();
-
-		ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.mainExpandableListView);
-		
-		expandableListView.setAdapter(expandableAdapter);
-
-		
-		
-		
-	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -110,7 +93,6 @@ public class AllViewActivity extends Activity {
 				}
 			});
 		}
-		
 	}
 	
 	@Override
@@ -118,45 +100,7 @@ public class AllViewActivity extends Activity {
 		super.onDestroy();
 		progressDialog.dismiss();
 	}
-	
-	
 
-//	@Override
-//	protected void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_all_view);
-//		
-//		
-//		TabHost tabHost = (TabHost) findViewById(R.id.myTabHost);
-//		this.CreateLoginAlert();
-//
-//		this.initExpandableData();
-//
-//		ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.mainExpandableListView);
-//		
-//		expandableListView.setAdapter(expandableAdapter);
-//
-//		// needed
-//		tabHost.setup();
-//
-//		tabHost.addTab(tabHost.newTabSpec("tab1")
-//				.setIndicator("tab1 indicator").setContent(R.id.view1));
-//		tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("tab2")
-//				.setContent(R.id.view2));
-//		tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("tab3")
-//				.setContent(R.id.view3));
-//		
-//		Button refreshButton = (Button) findViewById(R.id.refreshButton);
-//		refreshButton.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View arg0) {
-//				System.out.println("before");
-//				new Thread(new UpdateDataRunnable()).start();
-//				System.out.println("after");
-//			}
-//		});
-//
-//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -165,166 +109,43 @@ public class AllViewActivity extends Activity {
 		return true;
 	}
 
-	private void initExpandableData() {
-		groupData = new ArrayList<String>();
-		childrenData = new ArrayList<List<String>>();
-		
-		new Thread(new InitDataRunnable()).start();
-	}
-	
-	public void addInfo(String p,String[] c){  
-		groupData.add(p);  
-          
-        List<String> item = new ArrayList<String>();  
-          
-        for(int i=0;i<c.length;i++){  
-            item.add(c[i]);  
-        }  
-          
-        childrenData.add(item);  
-    }
-	
-	private class InitDataRunnable implements Runnable {
-		@Override
-		public void run() {
-			childrenData.clear();
-			groupData.clear();
-			try {
-				JenkinsJsonParser jenkinsJsonParser = new JenkinsJsonParser();
-				List<JenkinsView> jenkinsViewList = jenkinsJsonParser.getViewList(jenkinsURL);
-				for(JenkinsView jenkinsView : jenkinsViewList) {
-					groupData.add(jenkinsView.getName());
-					List<JenkinsProject> projectList = jenkinsJsonParser.getProjectListFromView(jenkinsView.getUrl());
-					List<String> projectNameList = new ArrayList<String>();
-					for(JenkinsProject jenkinsProject : projectList) {
-						projectNameList.add(jenkinsProject.getName());
-					}
-					childrenData.add(projectNameList);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
 
 	public void update(List<JenkinsView> jenkinsViewList) {
-		childrenData.clear();
-		groupData.clear();
-		try {
-			for(JenkinsView jenkinsView : jenkinsViewList) {
-				groupData.add(jenkinsView.getName());
-				List<JenkinsProject> projectList = jenkinsView.getJenkinsProjectList();
-				List<String> projectNameList = new ArrayList<String>();
-				for(JenkinsProject jenkinsProject : projectList) {
-					projectNameList.add(jenkinsProject.getName());
-				}
-				childrenData.add(projectNameList);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		childrenData.clear();
+//		groupData.clear();
+//		try {
+//			for(JenkinsView jenkinsView : jenkinsViewList) {
+//				groupData.add(jenkinsView.getName());
+//				List<JenkinsProject> projectList = jenkinsView.getJenkinsProjectList();
+//				List<String> projectNameList = new ArrayList<String>();
+//				for(JenkinsProject jenkinsProject : projectList) {
+//					projectNameList.add(jenkinsProject.getName());
+//				}
+//				childrenData.add(projectNameList);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
-		expandableAdapter.notifyDataSetChanged();
+//		expandableAdapter.notifyDataSetChanged();
 	}
 	
-	private class UpdateDataRunnable implements Runnable {
-		@Override
-		public void run() {
-			Message message = new Message();
-			message.what = 1;
-			
-			JenkinsJsonParser jenkinsJsonParser = new JenkinsJsonParser();
-			List<JenkinsView> jenkinsViewList = jenkinsJsonParser.getViewList(jenkinsURL);
-			
-			message.obj = jenkinsViewList;
-			
-			handler.sendMessage(message);
-		}
-	}
+//	private class UpdateDataRunnable implements Runnable {
+//		@Override
+//		public void run() {
+//			Message message = new Message();
+//			message.what = 1;
+//			
+//			JenkinsJsonParser jenkinsJsonParser = new JenkinsJsonParser();
+//			List<JenkinsView> jenkinsViewList = jenkinsJsonParser.getViewList(jenkinsURL);
+//			
+//			message.obj = jenkinsViewList;
+//			
+//			handler.sendMessage(message);
+//		}
+//	}
+	
+	
 
-	private class ExpandableAdapter extends BaseExpandableListAdapter {
-
-		@Override
-		public Object getChild(int arg0, int arg1) {
-			return childrenData.get(arg0).get(arg1);
-		}
-
-		@Override
-		public long getChildId(int arg0, int arg1) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public View getChildView(int arg0, int arg1, boolean arg2, View arg3,
-				ViewGroup arg4) {
-			TextView myText = null;
-			if (arg3 != null) {
-				myText = (TextView) arg3;
-				myText.setText(childrenData.get(arg0).get(arg1));
-			} else {
-				myText = createView(childrenData.get(arg0).get(arg1));
-			}
-			return myText;
-		}
-
-		@Override
-		public int getChildrenCount(int arg0) {
-			return childrenData.get(arg0).size();
-		}
-
-		@Override
-		public Object getGroup(int arg0) {
-			return groupData.get(arg0);
-		}
-
-		@Override
-		public int getGroupCount() {
-			return groupData.size();
-		}
-
-		@Override
-		public long getGroupId(int arg0) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public View getGroupView(int arg0, boolean arg1, View arg2,
-				ViewGroup arg3) {
-			TextView myText = null;
-			if (arg2 != null) {
-				myText = (TextView) arg2;
-				myText.setText(groupData.get(arg0));
-			} else {
-				myText = createView(groupData.get(arg0));
-			}
-			return myText;
-		}
-
-		@Override
-		public boolean hasStableIds() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean isChildSelectable(int arg0, int arg1) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		private TextView createView(String content) {
-			AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(
-					ViewGroup.LayoutParams.FILL_PARENT, 80);
-			TextView myText = new TextView(AllViewActivity.this);
-			myText.setLayoutParams(layoutParams);
-			myText.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-			myText.setPadding(80, 0, 0, 0);
-			myText.setText(content);
-			return myText;
-		}
-
-	}
 }
 
--- view4jenkins/src/com/k/qing/view4jenkins/AllViewHandler.java
+++ view4jenkins/src/com/k/qing/view4jenkins/AllViewHandler.java
@@ -2,31 +2,28 @@ package com.k.qing.view4jenkins;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.BaseExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.TabHost;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.k.qing.view4jenkins.bean.JenkinsProject;
 import com.k.qing.view4jenkins.bean.JenkinsView;
 import com.k.qing.view4jenkins.util.JenkinsJsonParser;
 
 public class AllViewHandler extends Handler {
 	private final WeakReference<Activity> mActivity;
 	
-	final ExpandableAdapter expandableAdapter = new ExpandableAdapter();
-	
-	private List<String> groupData;
-	private List<List<String>> childrenData;
+	private List<Map<String, Object>> mData = new ArrayList<Map<String,Object>>();
 	
     public AllViewHandler(Activity activity) {
         mActivity = new WeakReference<Activity>(activity);
@@ -40,32 +37,10 @@ public class AllViewHandler extends Handler {
 			case 0:
 				AllViewActivity.progressDialog.dismiss();
 				activity.setContentView(R.layout.activity_all_view);
-				TabHost tabHost = (TabHost)activity.findViewById(R.id.myTabHost);
-				
-				tabHost.setup();
-
-				tabHost.addTab(tabHost.newTabSpec("tab1")
-						.setIndicator("Jobs").setContent(R.id.view1));
-				tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("tab2")
-						.setContent(R.id.view2));
-				tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("tab3")
-						.setContent(R.id.view3));
-				
+				ListView expandableListView = (ListView) activity.findViewById(R.id.mainListView);
+				MyAdapter listAdapter = new MyAdapter(activity);
+				expandableListView.setAdapter(listAdapter);
 				this.initExpandableData();
-				
-				ExpandableListView expandableListView = (ExpandableListView) activity.findViewById(R.id.mainExpandableListView);
-				
-				expandableListView.setAdapter(expandableAdapter);
-				
-//				Button refreshButton = (Button) activity.findViewById(R.id.refreshButton);
-//				refreshButton.setOnClickListener(new OnClickListener() {
-//					@Override
-//					public void onClick(View arg0) {
-//						System.out.println("before");
-//						new Thread(new UpdateDataRunnable()).start();
-//						System.out.println("after");
-//					}
-//				});
 				return;
 			default:
 				break;
@@ -76,40 +51,25 @@ public class AllViewHandler extends Handler {
     }
     
     private void initExpandableData() {
-		groupData = new ArrayList<String>();
-		childrenData = new ArrayList<List<String>>();
-		
 		new Thread(new InitDataRunnable()).start();
 	}
-	
-	public void addInfo(String p,String[] c){  
-		groupData.add(p);  
-          
-        List<String> item = new ArrayList<String>();  
-          
-        for(int i=0;i<c.length;i++){  
-            item.add(c[i]);  
-        }  
-          
-        childrenData.add(item);  
-    }
 	
 	private class InitDataRunnable implements Runnable {
 		@Override
 		public void run() {
-			childrenData.clear();
-			groupData.clear();
+			if(mData == null) {
+				mData = new ArrayList<Map<String,Object>>();
+			} else {
+				mData.clear();
+			}
+			
 			try {
 				JenkinsJsonParser jenkinsJsonParser = new JenkinsJsonParser();
 				List<JenkinsView> jenkinsViewList = jenkinsJsonParser.getViewList(AllViewActivity.jenkinsURL);
 				for(JenkinsView jenkinsView : jenkinsViewList) {
-					groupData.add(jenkinsView.getName());
-					List<JenkinsProject> projectList = jenkinsJsonParser.getProjectListFromView(jenkinsView.getUrl());
-					List<String> projectNameList = new ArrayList<String>();
-					for(JenkinsProject jenkinsProject : projectList) {
-						projectNameList.add(jenkinsProject.getName());
-					}
-					childrenData.add(projectNameList);
+					Map<String, Object> viewMap = new HashMap<String, Object>();
+					viewMap.put("name", jenkinsView.getName());
+					mData.add(viewMap);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -117,89 +77,53 @@ public class AllViewHandler extends Handler {
 		}
 	}
 	
-	private class ExpandableAdapter extends BaseExpandableListAdapter {
-
-		@Override
-		public Object getChild(int arg0, int arg1) {
-			return childrenData.get(arg0).get(arg1);
-		}
-
-		@Override
-		public long getChildId(int arg0, int arg1) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public View getChildView(int arg0, int arg1, boolean arg2, View arg3,
-				ViewGroup arg4) {
-			TextView myText = null;
-			if (arg3 != null) {
-				myText = (TextView) arg3;
-				myText.setText(childrenData.get(arg0).get(arg1));
-			} else {
-				myText = createView(childrenData.get(arg0).get(arg1));
-			}
-			return myText;
-		}
-
-		@Override
-		public int getChildrenCount(int arg0) {
-			return childrenData.get(arg0).size();
-		}
-
-		@Override
-		public Object getGroup(int arg0) {
-			return groupData.get(arg0);
-		}
-
-		@Override
-		public int getGroupCount() {
-			return groupData.size();
-		}
-
-		@Override
-		public long getGroupId(int arg0) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public View getGroupView(int arg0, boolean arg1, View arg2,
-				ViewGroup arg3) {
-			TextView myText = null;
-			if (arg2 != null) {
-				myText = (TextView) arg2;
-				myText.setText(groupData.get(arg0));
-			} else {
-				myText = createView(groupData.get(arg0));
-			}
-			return myText;
-		}
-
-		@Override
-		public boolean hasStableIds() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean isChildSelectable(int arg0, int arg1) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		private TextView createView(String content) {
-			AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(
-					ViewGroup.LayoutParams.FILL_PARENT, 80);
-			TextView myText = new TextView(mActivity.get());
-			myText.setLayoutParams(layoutParams);
-			myText.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-			myText.setPadding(80, 0, 0, 0);
-			myText.setText(content);
-			return myText;
-		}
-
+	public final class ViewHolder {
+		public TextView name;
 	}
+	
+	public class MyAdapter extends BaseAdapter{
+		 
+        private LayoutInflater mInflater;
+         
+         
+        public MyAdapter(Context context){
+            this.mInflater = LayoutInflater.from(context);
+        }
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+ 
+        @Override
+        public Object getItem(int arg0) {
+            return null;
+        }
+ 
+        @Override
+        public long getItemId(int arg0) {
+            return 0;
+        }
+ 
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+             
+            ViewHolder holder = null;
+            if (convertView == null) {
+                 
+                holder=new ViewHolder();  
+                 
+                convertView = mInflater.inflate(R.layout.list_item, null);
+                holder.name = (TextView)convertView.findViewById(R.id.name);
+                convertView.setTag(holder);
+                 
+            }else {
+                holder = (ViewHolder)convertView.getTag();
+            }
+             
+            holder.name.setText((String)mData.get(position).get("name"));
+            return convertView;
+        }
+         
+    }
 }
 
